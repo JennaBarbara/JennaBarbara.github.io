01583c188ac2b4f597579d3dbde1f6330b2f3199
jennabarbara <jennabarbara@gmail.com>
2025-10-13 21:58:37 -0400

    added content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,51 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React from "react";
+import { BackgroundGradientAnimation } from "@/components/ui/shadcn-io/background-gradient-animation";
+import { Github } from 'lucide-react';
+
+export default function BackgroundGradientAnimationDemo() {
+  const backgroundStart = "rgb(173, 152, 177)"
+  const backgroundEnd = "rgb(190, 209, 255)"
+  const colorA = "220, 224, 233"
+  const colorB =  "217, 228, 255"
+  const colorC = "220, 224, 233"
+  const colorD = "217, 228, 255"
+  const colorE = "220, 224, 233"
+
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+    <BackgroundGradientAnimation
+          gradientBackgroundStart = {backgroundStart}
+          gradientBackgroundEnd = {backgroundEnd}
+          firstColor = {colorA}
+          secondColor = {colorB}
+          thirdColor = {colorC}
+          fourthColor = {colorE}
+          fifthColor = {colorB}
+          pointerColor = {colorD}
+          blendingValue = "hard-light"
+    >
+      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 text-3xl text-center">
+       <div className="grid grid-cols-1 gap-4 max-w-md">
+        <h1 className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-slate-800/20 to-slate-800/80 text-7xl">
+          Jenna Christensen
+        </h1>
+        <p className="text-slate-800/80 text-xl">
+          Toronto Based Frontend Engineer
+        </p> 
+          <a className="mt-6 bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://www.linkedin.com/in/jenna-christensen-0731b1b1/">
+            LinkedIn
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+             <a className="bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://github.com/JennaBarbara">
+           GitHub
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+             <a className="bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://codeberg.org/JennaBarbara">
+            Codeberg
+          </a>
+        
+       </div>
+      </div>
+    </BackgroundGradientAnimation>
   );
 }
