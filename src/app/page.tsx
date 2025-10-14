bac28eee552436f32719ba6374f0b2fefd1a256f
jennabarbara <jennabarbara@gmail.com>
2025-10-13 22:16:08 -0400

    updated colour scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import { BackgroundGradientAnimation } from "@/components/ui/shadcn-io/backgroun
 import { Github } from 'lucide-react';
 
 export default function BackgroundGradientAnimationDemo() {
-  const backgroundStart = "rgb(173, 152, 177)"
-  const backgroundEnd = "rgb(190, 209, 255)"
-  const colorA = "220, 224, 233"
+  const backgroundStart = "rgb(96, 134, 199)"
+  const backgroundEnd = "rgb(199, 210, 240)"
+  const colorA = "165, 202, 216"
   const colorB =  "217, 228, 255"
-  const colorC = "220, 224, 233"
-  const colorD = "217, 228, 255"
-  const colorE = "220, 224, 233"
+
 
 
   return (
@@ -20,19 +18,19 @@ export default function BackgroundGradientAnimationDemo() {
           gradientBackgroundEnd = {backgroundEnd}
           firstColor = {colorA}
           secondColor = {colorB}
-          thirdColor = {colorC}
-          fourthColor = {colorE}
+          thirdColor = {colorA}
+          fourthColor = {colorA}
           fifthColor = {colorB}
-          pointerColor = {colorD}
-          blendingValue = "hard-light"
+          pointerColor = {colorA}
+          blendingValue = "soft-light"
     >
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 text-3xl text-center">
        <div className="grid grid-cols-1 gap-4 max-w-md">
-        <h1 className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-slate-800/20 to-slate-800/80 text-7xl">
+        <h1 className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/40 to-white/90 text-7xl">
           Jenna Christensen
         </h1>
-        <p className="text-slate-800/80 text-xl">
-          Toronto Based Frontend Engineer
+        <p className="text-white text-xl">
+          Toronto Based Software Engineer
         </p> 
           <a className="mt-6 bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://www.linkedin.com/in/jenna-christensen-0731b1b1/">
             LinkedIn
