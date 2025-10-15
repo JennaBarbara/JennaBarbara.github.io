994b342cb3151fc83e217e6d80d9ab0c247cf0d8
jennabarbara <jennabarbara@gmail.com>
2025-10-15 10:56:52 -0400

    add headshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import React from "react";
 import { BackgroundGradientAnimation } from "@/components/ui/shadcn-io/background-gradient-animation";
-import { Github } from 'lucide-react';
+import headshot from "./assets/IMG_8937 2.png";
+import Image from 'next/image'
 
 export default function BackgroundGradientAnimationDemo() {
   const backgroundStart = "rgb(96, 134, 199)"
@@ -26,19 +27,28 @@ export default function BackgroundGradientAnimationDemo() {
     >
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 text-3xl text-center">
        <div className="grid grid-cols-1 gap-4 max-w-md">
-        <h1 className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/40 to-white/90 text-7xl">
+        <div className=" flex items-center justify-center">
+           <Image
+            className="rounded-full"
+            src={headshot}
+            width={250}
+            height={250}
+            alt="headshot"
+          />
+        </div>
+        <h1 className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/40 to-white/90 text-5xl">
           Jenna Christensen
         </h1>
         <p className="text-white text-xl">
           Toronto Based Software Engineer
         </p> 
-          <a className="mt-6 bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://www.linkedin.com/in/jenna-christensen-0731b1b1/">
+          <a className="mt-6 bg-cyan-50/50 hover:bg-cyan-50 focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://www.linkedin.com/in/jenna-christensen-0731b1b1/">
             LinkedIn
           </a>
-             <a className="bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://github.com/JennaBarbara">
+             <a className="bg-cyan-50/50 hover:bg-cyan-50 focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://github.com/JennaBarbara">
            GitHub
           </a>
-             <a className="bg-white/50 hover:bg-white focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://codeberg.org/JennaBarbara">
+             <a className="bg-cyan-50/50 hover:bg-cyan-50 focus:outline-offset-2 text-slate-500 font-bold py-2 px-4 rounded text-md" href="https://codeberg.org/JennaBarbara">
             Codeberg
           </a>
         
