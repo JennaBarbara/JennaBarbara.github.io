410bc6d7e7066bffa025c8ebef128edc11c92b92
jennabarbara <jennabarbara@gmail.com>
2025-11-06 23:11:21 -0500

    content updates

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -10,7 +10,42 @@ return (
         <p>Hi, how are you?  I&apos;m Jenna, a software engineer living in downtown Toronto. I&apos;ve been working as a frontend leaning fullstack developer at startups and growth-stage companies for the past few years. Those are the kind of roles I flourish in. I thrive when working in multidisciplinary teams where I can interface directly with stakeholders to create optimal solutions to match the needs of end users. 
 
         </p>
+  
+        
+
+
+    </Card>
+    <Card title="Personal Projects">
+      <div className="flex flex-col gap-2">
+        <h3 className="text-lg font-medium">
+          The Freesewing Sophie Slip 
+        </h3>
         <p>
+          Freesewing is an open-source platform for generating made-to-measure sewing patterns. The designs the patterns are based on are written as javascript code. I wanted to sew myself a slip dress, so I decided to try using the FreeSewing API to draft a new design. The Sophie Slip can be found on the official FreeSewing website,  <a className="text-slate-600 font-bold" href="https://freesewing.eu/designs/sophie/">here</a> and  a branch with my code contributions can be found <a className="text-slate-600 font-bold" href="https://codeberg.org/JennaBarbara/freesewing/src/branch/sophie-slip-design">here</a>. 
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h3 className="text-lg font-medium">
+          Raccoons to Riches
+        </h3>
+        <p>
+          I made all of the art assets and some limited UI design and code contributions on this upcoming videogame, built with the Godot Engine. It&apos;s a roguelike deckbuilder about running an item shop in a fantasy setting. The steam page can be found <a className="text-slate-600 font-bold" href="https://store.steampowered.com/app/3778620/Raccoons_to_Riches/">here</a>. 
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h3 className="text-lg font-medium">
+          This Is Your Life
+        </h3>
+        <p>
+        This project was a simple web app for automatically generating a character backstory following the “This is your Life” rules from the supplementary D&D sourcebook “Xanathar&apos;s Guide to Everything 5e”. I made this back when I was first learning Angular, so it&apos;s pretty barebones, but it&apos;s still a fun little tool if you&apos;re into tabletop RPGs. The app can be found <a className="text-slate-600 font-bold" href="https://jennabarbara.github.io/This-Is-Your-Life/">here</a>. 
+        </p>
+      </div>
+
+    </Card>
+    <Card title="Hobbies">
+       <p>
           Outside of my professional life, I find myself invested in a broad range of hobbies. Lately I&apos;ve been into: 
         </p>
         
@@ -50,36 +85,6 @@ return (
             
          </div>
          </div>
-
-
-    </Card>
-    <Card title="Personal Projects">
-      <div className="flex flex-col gap-2">
-        <h3 className="text-lg font-medium">
-          The Freesewing Sophie Slip 
-        </h3>
-        <p>
-          Freesewing is an open-source platform for generating made-to-measure sewing patterns. The designs the patterns are based on are written as javascript code. I wanted to sew myself a slip dress, so I decided to try using the FreeSewing API to draft a new design. I&apos;m currently working on getting the Sophie Slip code added to the official FreeSewing website, but in the meantime a preview deployment can be found <a className="text-slate-600 font-bold" href="https://fs-preview.sinerva.eu/designs/sophie/">here</a> and  a branch with my code contributions can be found <a className="text-slate-600 font-bold" href="https://codeberg.org/JennaBarbara/freesewing/src/branch/sophie-slip-design">here</a>. 
-        </p>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <h3 className="text-lg font-medium">
-          Raccoons to Riches
-        </h3>
-        <p>
-          I made all of the art assets and some limited UI design and code contributions on this upcoming videogame, built with the Godot Engine. It&apos;s a roguelike deckbuilder about running an item shop in a fantasy setting. The steam page can be found <a className="text-slate-600 font-bold" href="https://store.steampowered.com/app/3778620/Raccoons_to_Riches/">here</a>. 
-        </p>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <h3 className="text-lg font-medium">
-          This Is Your Life
-        </h3>
-        <p>
-        This project was a simple web app for automatically generating a character backstory following the “This is your Life” rules from the supplementary D&D sourcebook “Xanathar&apos;s Guide to Everything 5e”. I made this back when I was first learning Angular, so it&apos;s pretty barebones, but it&apos;s still a fun little tool if you&apos;re into tabletop RPGs. The app can be found <a className="text-slate-600 font-bold" href="https://jennabarbara.github.io/This-Is-Your-Life/">here</a>. 
-        </p>
-      </div>
 
     </Card>
     </div>
